8cb927f80f63f499ffdfd849bdee7ffa87e9b58d
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-05-10 00:41:02 +0200

    chore: setup style guide and assets

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+const Colors = {
+	'primary-100': 'hsl(0, 0%, 63%)',
+	'primary-200': 'hsl(0, 0%, 27%)',
+	black: 'hsl(0, 0%, 0%)',
+	white: 'hsl(0, 0%, 100%)',
+}
+
+export default {
+	Colors,
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-100': 'hsl(0, 0%, 63%)',
+				'primary-200': 'hsl(0, 0%, 27%)',
+				black: 'hsl(0, 0%, 0%)',
+				white: 'hsl(0, 0%, 100%)',
+			},
+			fontSize: {
+				body: '12px',
+			},
+			fontFamily: {
+				sans: ['League Spartan', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
